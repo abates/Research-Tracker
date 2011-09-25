a9b6b25e065f20463662245c34c0839f363c8d1a
Andrew Bates <abates@omeganetserv.com>
2011-09-25 18:58:34 +0000

    Added percent_complete method in order to return zero if no value has been set

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,11 @@ class Task < ActiveRecord::Base
   belongs_to :project
   has_many :notes, :as => :notable
   textile :description
+
+  def percent_complete
+    p = read_attribute(:percent_complete)
+    (p.nil? || p.blank? ? 0 : p)
+  end
 end
+
+

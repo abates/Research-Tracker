eb14da87780ef51769e22851a94305b4833e040d
Andrew Bates <abates@omeganetserv.com>
2011-09-25 18:57:19 +0000

    Added helper to expand term_names into a comma delimited string

--- app/helpers/papers_helper.rb
+++ app/helpers/papers_helper.rb
@@ -1,4 +1,8 @@
 module PapersHelper
+  def term_names paper
+    paper.term_names.join(', ');
+  end
+
   def term_links paper
     return 'No terms associated with this paper' if paper.term_names.length == 0
     paper.term_names.map {|t| link_to t, term_path(t) }.join(', ').html_safe
